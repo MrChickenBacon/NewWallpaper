8004ebf0d3315d8fa5d79885ffabbe9e3a71d569
Christoffer Hellenes <34237142+MrChickenBacon@users.noreply.github.com>
2018-12-19 10:00:50 +0100

    Initial commit

--- NewWallpaper/Program.cs
+++ NewWallpaper/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,8 +10,24 @@ namespace NewWallpaper
 {
     class Program
     {
+
+        [DllImport("User32", CharSet = CharSet.Auto)]
+        public static extern int SystemParametersInfo(int uiAction, int uiParam,
+            string pvParam, uint fWinIni);
+
+        public static string Path = $"{Environment.GetFolderPath(Environment.SpecialFolder.UserProfile)}";
+
+        static Random random = new Random();
+
+        static int Numbers()
+        {
+            int number = random.Next(1, 500);
+            return number;
+        }
+
         static void Main(string[] args)
         {
+            SystemParametersInfo(0x0014, 0, $@"{Path}\desktop\wallpapers\{Numbers()}.jpg", 0x0001);
         }
     }
 }
